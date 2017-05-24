371b72aa3e676d4290043bedfb609f41c0f03986
Brad Edwards <bledwards1@gmail.com>
2017-05-24 10:23:06 -0500

    Added logging
    Added logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
+logger = Logger.new(STDERR)
+logger = Logger.new(STDOUT)
+
+logger = Logger.new('logfile.log')
+
+file = File.open('foo.log', File::WRONLY | File::APPEND)
+# To create new (and to remove old) logfile, add File::CREAT like;
+#   file = open('foo.log', File::WRONLY | File::APPEND | File::CREAT)
+logger = Logger.new(file)
+
+logger = Logger.new('foo.log', 10, 1024000)
+
+logger = Logger.new('foo.log', 'daily')
+logger = Logger.new('foo.log', 'weekly')
+logger = Logger.new('foo.log', 'monthly')
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
